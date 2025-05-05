9112de510d3e3e0c44853f67da459f2f4ec5c23f
zia <zia166@gmail.com>
2025-05-05 18:57:20 +0100

    MongoDB Connection Issue

--- Bookify_Hub_Services/Bookify_Hub_Services/db.py
+++ Bookify_Hub_Services/Bookify_Hub_Services/db.py
@@ -2,7 +2,14 @@ from django.conf import settings
 from pymongo import MongoClient
 import certifi
 
-client = MongoClient(settings.ATLAS_CONNECTION_STRING, tlsCAFile=certifi.where())
+client = MongoClient(
+    settings.ATLAS_CONNECTION_STRING,
+    tlsCAFile=certifi.where(),
+    tlsAllowInvalidCertificates=False,
+    tlsAllowInvalidHostnames=False,
+    retryWrites=True,
+    w='majority'
+)
 
 # Connect to MongoDB
 dbconn = client[settings.MONGO_DB]

559c4374b0205165dd5eb27dc66b94c614299513
zia <zia166@gmail.com>
2025-05-11 22:45:53 +0100

    MongoDB Connection Issue-5

--- Bookify_Hub_Services/Bookify_Hub_Services/db.py
+++ Bookify_Hub_Services/Bookify_Hub_Services/db.py
@@ -1,21 +1,8 @@
 from django.conf import settings
 from pymongo import MongoClient
 import certifi
-import ssl
 
-client = MongoClient(
-    settings.ATLAS_CONNECTION_STRING,
-    tls=True,
-    tlsCAFile=certifi.where(),
-    tlsAllowInvalidCertificates=True,
-    tlsAllowInvalidHostnames=True,
-    retryWrites=True,
-    w='majority',
-    connectTimeoutMS=30000,
-    socketTimeoutMS=30000,
-    serverSelectionTimeoutMS=30000,
-    ssl_cert_reqs=ssl.CERT_NONE
-)
 
+client = MongoClient(settings.ATLAS_CONNECTION_STRING, tlsCAFile=certifi.where())
 # Connect to MongoDB
 dbconn = client[settings.MONGO_DB]
